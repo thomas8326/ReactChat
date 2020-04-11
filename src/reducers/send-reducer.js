edd91d83e1463cfc026fc41c8be741c94af8b687
Thomas Shih <tt7919@gmail.com>
2020-04-11 17:44:38 +0800

    new message reducer

--- src/reducers/send-reducer.js
+++ src/reducers/send-reducer.js
@@ -1,23 +1,36 @@
 import { SEND_MESSAGE, SEND_FILE, SEND_IMAGE } from "../constants/action-constants";
 
-export const sendReducer = (message = {}, action) => {
-    switch(action.type) {
-        case SEND_MESSAGE:
-            return {
-                userId: action.userId,
-                messageId: action.messageId,
-                message: action.message
-            }
-        case SEND_FILE:
-        case SEND_IMAGE:
-            return {
-                userId: action.userId,
-                messageId: action.messageId,
-                message: action.file
-            }
-        default:
-            return message;
+const textMessage = (action) => {
+    return {
+        userId: action.userId,
+        messageId: action.messageId,
+        message: action.message
     }
 }
 
-export default sendReducer;
+const fileMessage = (action) => {
+    return {
+        userId: action.userId,
+        messageId: action.messageId,
+        message: action.file
+    }
+}
+
+export const messages = (lastMessageState = [], action) => {
+    console.log(action);
+    switch (action.type) {
+        case SEND_MESSAGE:
+            return [
+                ...lastMessageState,
+                textMessage(action)
+            ];
+        case SEND_FILE:
+        case SEND_IMAGE:
+            return [
+                ...lastMessageState,
+                fileMessage(action)
+            ]
+        default:
+            return lastMessageState;
+    }
+}
