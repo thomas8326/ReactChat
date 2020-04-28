6367a387c5e02a5f596a57c86431eddcdeeb9be5
Thomas Shih <tt7919@gmail.com>
2020-04-28 21:01:43 +0800

    reuturn app

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Sender from './container/sender';
 import Conversation from './container/conversation';
 import { User } from './container/user';
 
-const App = () => (
-  <div>
-    <Conversation></Conversation>
-    <Sender></Sender>
-    <User></User>
+const App = () => {
+  return (
+    <div>
+      <Conversation></Conversation>
+      <Sender></Sender>
+      <User></User>
   </div>
-)
+  )
+}
 
 export default App;
