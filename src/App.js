28bb2f4860be4767a1d264289a41dfbd4d93b595
Thomas Shih <tt7919@gmail.com>
2020-07-29 23:51:32 +0800

    style: add chat room list

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import Sender from './container/sender';
 import Conversation from './container/conversation';
 import { User } from './container/user';
+import Rooms from './container/room';
 
 const App = () => {
   return (
-    <div>
-      <Conversation></Conversation>
-      <Sender></Sender>
-      <User></User>
+    <div className="chat">
+      <Rooms className="room"></Rooms>
+      <div className="conversation">
+        <Conversation></Conversation>
+        <Sender></Sender>
+        <User></User>
+      </div>
   </div>
   )
 }
